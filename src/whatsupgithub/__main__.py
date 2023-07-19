d9da9fdb5f12ecb8c625cf7516eaeae221b92df8
AoifeHughes <ahughes@turing.ac.uk>
2023-07-19 10:09:29 +0100

    Added some stuff for repo info scraping

--- src/whatsupgithub/__main__.py
+++ src/whatsupgithub/__main__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from os import environ
 
 import pandas as pd
-from github import Auth, Github
+from github import Auth, Github, UnknownObjectException
 from tqdm import tqdm
 
 DEFAULT_ORG = "alan-turing-institute"
@@ -15,6 +15,15 @@ def has_file(repo, filename):
     """Check if a file exists in a repo."""
     try:
         repo.get_contents(filename)
+        return True
+    except Exception:
+        return False
+
+
+def has_comment(repo, comment):
+    """Check if a comment exists in a repo."""
+    try:
+        repo.get_contents(comment)
         return True
     except Exception:
         return False
@@ -33,10 +42,81 @@ def parse_args():
         default=False,
         help="Include private repos in the stats",
     )
+    parser.add_argument("--repo", default=None, help="Get stats for a single repo")
     return vars(parser.parse_args())
 
 
-def to_table(repos):
+def check_user_is_in_org(g, org, user):
+    try:
+        g.get_user(user).get_organization_membership(org)
+    except UnknownObjectException:
+        return False
+    return True
+
+
+def get_all_contributors(repo, inc_non_code=True):
+    """Get all contributors to a repo."""
+    contributors = {}
+    for contributor in repo.get_contributors():
+        contributors[contributor.login] = ["code"]
+    if inc_non_code:
+        for issue in repo.get_issues():
+            if issue.user.login not in contributors:
+                contributors[issue.user.login] = ["issues"]
+            else:
+                if "issues" not in contributors[issue.user.login]:
+                    contributors[issue.user.login].append("issues")
+            for comment in issue.get_comments():
+                if comment.user.login not in contributors:
+                    contributors[comment.user.login] = ["comments"]
+                else:
+                    if "comments" not in contributors[comment.user.login]:
+                        contributors[comment.user.login].append("comments")
+    return contributors
+
+
+def github_to_timestamp(s):
+    return datetime.strptime(s.last_modified, "%a, %d %b %Y %H:%M:%S %Z")
+
+
+def get_all_commit_issue_comments_info(repo):
+    """Get all commit, issue and comment info from a repo.
+    Returns a dataframe with columns:
+    - user
+    - timestamp
+    - type (commit, issue, comment)
+    """
+    rows = []
+
+    print("Doing all commits")
+    for commit in tqdm(repo.get_commits()):
+        try:
+            row = []
+            row.append(commit.author.login)
+            row.append(github_to_timestamp(commit))
+            row.append("commit")
+            rows.append(row)
+        except AttributeError:
+            pass
+    print("Doing all issues and comments")
+    for issue in tqdm(repo.get_issues()):
+        row = []
+        row.append(issue.user.login)
+        row.append(issue.created_at.strftime("%a, %d %b %Y %H:%M:%S %Z"))
+        row.append("issue")
+        rows.append(row)
+
+        for comment in issue.get_comments():
+            row = []
+            row.append(comment.user.login)
+            row.append(comment.created_at.strftime("%a, %d %b %Y %H:%M:%S %Z"))
+            row.append("comment")
+            rows.append(row)
+
+    return pd.DataFrame(rows, columns=["user", "timestamp", "type"])
+
+
+def repo_info_to_table(repos):
     """
     Generates a CSV of repos where columns should include:
     - repo name
@@ -102,13 +182,18 @@ def main():
     auth = Auth.Token(token)
     g = Github(auth=auth)
     args = parse_args()
-    if not args["private"]:
-        repos = list(g.get_organization(args["org"]).get_repos())
+    if args["repo"] is not None:
+        repo = g.get_repo(args["repo"])
+        tbl = get_all_commit_issue_comments_info(repo)
+        tbl.to_csv("repo.csv")
     else:
-        repos = list(g.get_organization(args["org"]).get_repos(type="private"))
+        if not args["private"]:
+            repos = list(g.get_organization(args["org"]).get_repos())
+        else:
+            repos = list(g.get_organization(args["org"]).get_repos(type="private"))
 
-    tbl = to_table(repos)
-    tbl.to_csv("repos.csv")
+        tbl = repo_info_to_table(repos)
+        tbl.to_csv("repos.csv")
 
 
 if __name__ == "__main__":
